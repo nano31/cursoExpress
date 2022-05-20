500299998625db94856fbfbc3c1ae8eac7810af9
Franco Garcia Carril <78042300+nano31@users.noreply.github.com>
2022-05-20 00:26:32 -0300

    some files

---
+++ index.js
@@ -0,0 +1,44 @@
+//servidor creado con NODE
+// const http = require('http');
+
+// const server = http.createServer((/*req,*/ res =>{
+//     res.status = 200;
+//     res.setHeader('Content-type', 'text/palin');
+//     res.end('hello world');
+// }));
+
+// server.listen(3000, () => {
+//     console.log('Serve on port 3000');
+// });
+
+//CREACION DEL SERVIDOR CON EXPRESS
+
+const express = require('express');
+const app = express(); //permite ejecutar el modulo express, devuelve un modulo en formato json
+
+app.use(express.json()); //hace que express pueda entender objetos json
+
+app.get('/user',(req, res) =>{ //permite responder a las peticiones (ruta)
+    res.json({ //devuelve un objeto js
+        username: 'Cameron',
+        lastname: 'How'
+    }); 
+});
+
+app.post('/user/:id', (req, res) =>{ //crea la ruta /about, se pueden crear tantas rutas como se quiera
+    console.log(req.body);
+    console.log(req.params);
+    res.send('Peticion POST');
+});
+
+app.put('/contact', (req, res) =>{
+    res.send('Peticion PUT');
+});
+
+app.delete('/test',(req, res) => {
+    res.send('Peticion DELETE');
+});
+
+app.listen(5000, () => {
+    console.log('Server on port 5000');
+});
